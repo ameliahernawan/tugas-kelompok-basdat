e22467da8a17d0d93c389dde69554c294159972a
nauvalcalya <nauvalhc@gmail.com>
2021-12-20 22:31:36 +0700

    Update Style_Halaman Utama

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>basdat kel</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600&display=swap" rel="stylesheet">
+</head>
+<body>
+    <nav>
+        <div class="wrapper-menu">
+            <!--logo tutorin, belum pasti apa yang bagus ntar diganti aja-->
+            <!-- <div class="logo"><a href="">Tutorin</a></div> -->
+            <div class="menu">
+                <ul>
+                    <!--untuk menu yang diatas-->
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#aboutus">About us</a></li>
+                    <li><a href="#testimoni">Testimoni</a></li>
+                    <li><a href="#harga">Harga</a></li>
+                    <li><a href="#tutor">Tutor</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="wrapper">
+        <section id="home">
+            <div class="kolom">
+                <h1 class="deskripsi">Butuh tutor pribadi untuk belajar?</h1>
+                <p><span>Tutorin</span> hadir sebagai sarana untuk mahasiswa yang kesulitan memahami pelajaran selama masa sekolah online ini.</p>
+                <!--dari sini ntar masuk ke halaman login-->
+                <a href="" class="tombol">Coba Sekarang</a>
+            </div>
+            <!-- <img src="E:\ilkom\Basdat\tugas kelompok\home.png"> -->
+        </section>
+        <section id="image_hero">
+            <img src="img/hero-image.png" alt="">
+        </section>
+    </div>
+</body>
+</html>
