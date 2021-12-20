1b5cce8e63dd6b37d2e9a9d1951c2a1a73f7eea7
ahmedsiddiqar <70643918+ahmedsiddiqar@users.noreply.github.com>
2021-12-21 00:26:43 +0700

    Add files via upload

---
+++ daftar.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Formulir Pendaftaran Murid Baru</title>
+</head>
+
+<body>
+	<header>
+		<h3>Formulir Pendaftaran murid Baru</h3>
+	</header>
+
+	<form action="prosespendaftaran.php" method="POST">
+		<fieldset>
+		<p>
+			<label for="nama">Nama: </label>
+			<input type="text" name="nama" placeholder="nama lengkap" />
+		</p>
+		<p>
+			<label for="nim">NIM: </label>
+			<input type="text" name="nim" />
+		</p>
+		<p>
+			<label for="tutor 1">Tutor 1: </label>
+			<input type="text" name="tutor 1" />
+		</p>
+        <p>
+			<label for="tutor 2">Tutor 2: </label>
+			<input type="text" name="tutor 2" />
+		</p>
+        <p>
+			<label for="tutor 3">Tutor 3: </label>
+			<input type="text" name="tutor 3" />
+		</p>
+        <p>
+			<label for="tutor 4">Tutor 4: </label>
+			<input type="text" name="tutor 4" />
+		</p>
+        <p>
+			<label for="tutor 5">Tutor 5: </label>
+			<input type="text" name="tutor 5" />
+		</p>
+
+		</fieldset>
+	</form>
+
+	</body>
+</html>
