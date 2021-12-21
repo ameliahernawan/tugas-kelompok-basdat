102253672ca22070e6614f24c2ed42c8a9d3fb00
ahmedsiddiqar <70643918+ahmedsiddiqar@users.noreply.github.com>
2021-12-21 22:10:33 +0700

    Update proseshapus.php

--- proseshapus.php
+++ proseshapus.php
@@ -1,24 +1,13 @@
 <?php
 include("config.php");
-// cek apakah tombol daftar sudah diklik atau blum?
 if(isset($_POST['hapus'])){
-
-	// ambil data dari formulir
 	$nim = $_POST['nim'];
-	// $tutor = $_POST['tutor'];
-
-
-	$query = pg_query("DELETE FROM mahasiswa WHERE NIM='$nim'");
-  
-	  // apakah query simpan berhasil?
-	  if( $query==TRUE ) {
-		  $up = pg_query("DELETE FROM mahasiswa SET IDtutor = Tutor.idtutor");
-		  // kalau berhasil alihkan ke halaman index.php dengan status=sukses
-		  header('Location: index.php?status=sukses');
-	  } else {
-		  // kalau gagal alihkan ke halaman indek.ph dengan status=gagal
-		  echo"gagal menghapus";
-	  }
+	$query = pg_query("DELETE FROM mahasiswa WHERE nim ='$nim'");
+    if($query) {
+        header ('Location: index.php');
+    } else {
+        die("gagal menghapus . . . ");
+    }
  
 } else {
 	die("Akses dilarang...");
