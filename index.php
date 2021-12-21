e9acb1f7bcb395d8779cd7ebeefe771df9defd69
nauvalcalya <nauvalhc@gmail.com>
2021-12-21 20:02:23 +0700

    menggunakan tmplate

--- index.php
+++ index.php
@@ -1,30 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>basdat kel</title>
-    <link rel="stylesheet" href="css/style.css">
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600&display=swap" rel="stylesheet">
-</head>
-<body>
-    <nav>
-        <div class="wrapper-menu">
-            <!--logo tutorin, belum pasti apa yang bagus ntar diganti aja-->
-            <!-- <div class="logo"><a href="">Tutorin</a></div> -->
-            <div class="menu">
-                <ul>
-                    <!--untuk menu yang diatas-->
-                    <li><img class="logo" src="img/logoTutor4u.png" alt=""></li>
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#aboutus">About us</a></li>
-                    <li><a href="#testimoni">Testimoni</a></li>
-                    <li><a href="#harga">Harga</a></li>
-                    <li><a href="#tutor">Tutor</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+<?php require_once "template/header.php" ?>
     <div class="wrapper">
         <section id="home">
             <div class="kolom">
@@ -39,5 +13,4 @@
             <img src="img/hero-image.png" alt="">
         </section>
     </div>
-</body>
-</html>
+<?php require_once "template/footer.php" ?>
