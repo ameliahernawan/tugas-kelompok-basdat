42ef5e6dad7915f75a6dfdbf8c7637e618ac8b37
nauvalcalya <nauvalhc@gmail.com>
2021-12-21 21:36:11 +0700

    Update edit

---
+++ edit.php
@@ -0,0 +1,54 @@
+<?php require_once "template/headContent.php" ?>
+<div class="container daftar">
+  <form class="left" action="prosesganti.php" method="POST">
+    <div>
+		<h1>Selamat Datang di <span>Tutor4u</span></h1>
+		<hr>
+		<div class="form-group">
+			<label for="nim">NIM</label>
+			<input type="text" name="nim" />
+		</div>
+		<div class="form-group">
+			<label for="tutor">Tutor</label>
+			<select name="tutor">
+				<option value="01A">Matematika-Tony Stark|-16:00-22:00</option>
+				<option value="02A">Matematika-Clint Barton|-16:00-22:00</option>
+				<option value="03A">Matematika-Vision|-16:00-22:00</option>
+				<option value="04A">Matematika-Nick Furry|-15:00-21:00</option>
+				<option value="05A">Matematika-Shuri|-15:00-21:00</option>
+				<option value="06B">Bahasa Indonesia-Natasha Romanoff|-16:00-22:00</option>
+				<option value="07B">Bahasa Indonesia-James Rhodes|-16:00-22:00</option>
+				<option value="08B">Bahasa Indonesia-Peter Quill|-15:00-21:00</option>
+				<option value="09B">Bahasa Indonesia-Gamora|-14:00-20:00</option>
+				<option value="10B">Bahasa Indonesia-Groot|-15:00-21:00</option>
+				<option value="11C">Bahasa Inggris-Thor Odinson|-16:00-22:00</option>
+				<option value="12C">Bahasa Inggris-Wanda Maximoff|-15:00-21:00</option>
+				<option value="13C">Bahasa Inggris-Bucky Barnes|-16:00-22:00</option>
+				<option value="14C">Bahasa Inggris-Carol Danvers|-16:00-22:00</option>
+				<option value="15C">Bahasa Inggris-Drax|-15:00-21:00</option>
+				<option value="16D">Kimia-Steve Rogers|-16:00-22:00</option>
+				<option value="17D">Kimia-Peter Parker|-14:00-20:00</option>
+				<option value="18D">Kimia-Yelena Belova|-16:00-22:00</option>
+				<option value="19D">Kimia-Maria Hill|-15:00-21:00</option>
+				<option value="20D">Kimia-Okoye|-16:00-22:00</option>
+				<option value="21E">Biologi-Bruce Banner|-16:00-22:00</option>
+				<option value="22E">Biologi-Mantis|-15:00-21:00</option>
+				<option value="23E">Biologi-Stephen Strange|-15:00-21:00</option>
+				<option value="24E">Biologi-Pietro Maximoff|-16:00-22:00</option>
+				<option value="25E">Biologi-Loki Odinson|-14:00-20:00</option>
+				<option value="26F">Fisika-Sam Wilson|-16:00-22:00</option>
+				<option value="27F">Fisika-Rocket|-15:00-21:00</option>
+				<option value="28F">Fisika-Nebula|-16:00-22:00</option>
+				<option value="29F">Fisika-TChalla|-15:00-21:00</option>
+				<option value="30F">Fisika-Scott Lang|-16:00-22:00</option>
+			</select>
+		</div>
+		<button type="submit" name="daftar">Edit</button>
+    </div>
+  </form>
+  <div class="right" style="align-items:center;">
+	  <img style="width: 90%;" src="img/SignUpbb.png" alt="">
+  </div>
+</div>
+<?php require_once "template/footer.php" ?>
+
--- index.php
+++ index.php
@@ -6,8 +6,10 @@
                 <p><span>Tutor4u</span> hadir sebagai sarana untuk mahasiswa yang kesulitan memahami pelajaran selama masa sekolah online ini.</p>
                 <!--dari sini ntar masuk ke halaman login-->
                 <a href="daftar.php" class="tombol">Coba Sekarang</a>
+                <h2 style="margin-top:20px;">Ganti Tutor?</h2>
+                <h2>Udah mau keluar nih?</h2>
             </div>
-            <!-- <img src="E:\ilkom\Basdat\tugas kelompok\home.png"> -->
+            
         </section>
         <section id="image_hero">
             <img src="img/hero-image.png" alt="">
