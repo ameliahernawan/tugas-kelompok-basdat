7a1e466b314d9c7e34d7381c8ca3edc9f253ff2b
ahmedsiddiqar <70643918+ahmedsiddiqar@users.noreply.github.com>
2021-12-21 21:19:30 +0700

    Add files via upload

---
+++ formganti.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+
+
+
+	<form action="prosesganti.php" method="POST">
+		<fieldset>
+		<p>
+			<label for="nim">NIM: </label>
+			<input type="text" name="nim" />
+		</p>
+		<p>
+			<label for="tutor">Tutor: </label>
+			<input type="text" name="tutor" />
+		</p>
+
+
+        <p>
+			<input type="submit" value="Daftar" name="daftar" />
+		</p>
+
+		</fieldset>
+	</form>
+
+
+	</body>
+</html>
