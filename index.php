a54b1b6afdf734c5cf1de6ef9074af3e6e528f5d
nauvalcalya <nauvalhc@gmail.com>
2021-12-21 21:45:13 +0700

    pencetan edit

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
                 <p><span>Tutor4u</span> hadir sebagai sarana untuk mahasiswa yang kesulitan memahami pelajaran selama masa sekolah online ini.</p>
                 <!--dari sini ntar masuk ke halaman login-->
                 <a href="daftar.php" class="tombol">Coba Sekarang</a>
-                <h2 style="margin-top:20px;">Ganti Tutor?</h2>
+                <h2 style="margin-top:20px;"><a href="edit.php">Ganti Tutor?</h2>
                 <h2>Udah mau keluar nih?</h2>
             </div>
             
