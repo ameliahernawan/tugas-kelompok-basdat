3d8200452ab857bd025ee8de663d2195e2f2d3b6
ahmedsiddiqar <70643918+ahmedsiddiqar@users.noreply.github.com>
2021-12-21 19:26:15 +0700

    Update prosespendaftaran.php

--- prosespendaftaran.php
+++ prosespendaftaran.php
@@ -9,17 +9,18 @@ if(isset($_POST['daftar'])){
 	$tutor = $_POST['tutor'];
 
 
-	$query = pg_query("INSERT INTO siswa (nama, nim, IDtutor) VALUEs ('$nama', '$nim','$tutor')");
+	$query = pg_query("INSERT INTO mahasiswa (username, NIM, IDtutor) VALUEs ('$nama', '$nim','$tutor')");
   
 	  // apakah query simpan berhasil?
 	  if( $query==TRUE ) {
+		  $up = pg_query("UPDATE mahasiswa SET pilihmatkul = namamatkul, pilihtutor = namatutor, mulai = start, selesai = finish FROM Tutor WHERE mahasiswa.IDtutor = Tutor.idtutor");
 		  // kalau berhasil alihkan ke halaman index.php dengan status=sukses
 		  header('Location: index.php?status=sukses');
 	  } else {
 		  // kalau gagal alihkan ke halaman indek.ph dengan status=gagal
 		  echo"gagal mendaftar";
 	  }
-
+ 
 } else {
 	die("Akses dilarang...");
 }
