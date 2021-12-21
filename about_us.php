12bf7a4fab1b079b8fb72bdaac098f2723982723
nauvalcalya <nauvalhc@gmail.com>
2021-12-21 20:02:37 +0700

    halaman about us

---
+++ about_us.php
@@ -0,0 +1,13 @@
+<?php require_once "template/header.php" ?>
+    <div class="wrapper about_us">
+        <div style="width: 50%;">
+            <img id="image_about_us" src="img/Gambar2.png" alt="">
+        </div>
+        <div class="body" style="width:50%;">
+            <h1>How <span>Tutor4U</span> Can Help You</h1>
+            <p>
+            <span>Tutor4u</span> hadir sebagai bimbingan belajar online agar kamu bisa belajar dimanapun. Didukung oleh para pengajar yang terbaik dibidangnya, kami dapat membantu membantu setiap permasalahan yang dialami mahasiswa seperti materi, tugas, hingga konsultasi tugas proyek akhir. Tutor online ini bersifat lebih intens karena kami menggunakan sistem pengajar privat, seperti memiliki guru pribadi di rumah sendiri.
+            </p>
+        </div>
+    </div>
+<?php require_once "template/footer.php" ?>
