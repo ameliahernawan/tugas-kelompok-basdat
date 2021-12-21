71ccb6f0133341e62dac6313c7c0b3634d4ec5b3
nauvalcalya <nauvalhc@gmail.com>
2021-12-21 20:01:56 +0700

    Heade Footer

---
+++ template/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>basdat kel</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600&display=swap" rel="stylesheet">
+</head>
+<body>
+    <nav>
+        <div class="wrapper-menu">
+            <!--logo tutorin, belum pasti apa yang bagus ntar diganti aja-->
+            <!-- <div class="logo"><a href="">Tutorin</a></div> -->
+            <div class="menu">
+                <img class="logo" src="img/logoTutor4u.png" alt="">
+                <ul>
+                    <!--untuk menu yang diatas-->
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#aboutus">About us</a></li>
+                    <li><a href="#testimoni">Testimoni</a></li>
+                    <li><a href="#harga">Harga</a></li>
+                    <li><a href="#tutor">Tutor</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
