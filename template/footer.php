6014f7bcec9ac6eccba2be44b61c500d96fa4fea
nauvalcalya <nauvalhc@gmail.com>
2021-12-21 20:05:53 +0700

    update footer

--- template/footer.php
+++ template/footer.php
@@ -1,2 +1,11 @@
 </body>
 </html>
+<?php if(isset($_GET['status'])): ?>
+	<p>
+		<?php
+			if($_GET['status'] == 'sukses'){
+				echo "Pendaftaran tutor berhasil!";
+			}
+		?>
+	</p>
+	<?php endif; ?>
